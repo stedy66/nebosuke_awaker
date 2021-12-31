aa6e3ae405b7ef8958283672fd9a6a44eea4fd4b
katonyonko <katonyonko@yahoo.co.jp>
2021-12-31 18:53:02 +0900

    mrreg_act更新

--- mrreg_act.php
+++ mrreg_act.php
@@ -1,14 +1,5 @@
 <?php
 session_start();
-echo $_POST["ROUTINE_NAME"];
-echo $_POST["DESCRIPTION"];
-echo $_POST["YOUTUBE"];
-if (isset($_POST["SHARED"])) {
-  echo 1;
-} else {
-  echo 0;
-}
-echo '<br>';
 $i=1;
 while (isset($_POST["STEP_ID".$i])) {
   echo $_POST["STEP_ID".$i].'<br>';
@@ -20,4 +11,25 @@ while (isset($_POST["STEP_ID".$i])) {
 include("funcs.php");
 sschk();
 $pdo = db_conn();
+
+$stmt = $pdo->prepare("INSERT INTO table1_1(USER_ID, ROUTINE_NAME, IMG_URL, DOWNLOAD_NUM, DESCRIPTION, YOUTUBE, SHARED, CREATE_DATE)VALUES(:USER_ID, :ROUTINE_NAME, :IMG_URL, 0, :DESCRIPTION, :YOUTUBE, :SHARED, sysdate())");
+$stmt->bindValue(':USER_ID', $_SESSION["USER_ID"], PDO::PARAM_STR);      //Integer（数値の場合 PDO::PARAM_INT)
+$stmt->bindValue(':ROUTINE_NAME', $_POST["ROUTINE_NAME"], PDO::PARAM_STR);      //Integer（数値の場合 PDO::PARAM_INT)
+$stmt->bindValue(':IMG_URL', "", PDO::PARAM_STR);      //Integer（数値の場合 PDO::PARAM_INT)
+$stmt->bindValue(':DESCRIPTION', $_POST["DESCRIPTION"], PDO::PARAM_STR);      //Integer（数値の場合 PDO::PARAM_INT)
+$stmt->bindValue(':YOUTUBE', $_POST["YOUTUBE"], PDO::PARAM_STR);      //Integer（数値の場合 PDO::PARAM_INT)
+//SHAREDにチェックが入っていたら1,入っていなかったら0
+if (isset($_POST["SHARED"])) {
+  $SHARED=1;
+} else {
+  $SHARED=0;
+}
+$stmt->bindValue(':SHARED', $SHARED, PDO::PARAM_INT);      //Integer（数値の場合 PDO::PARAM_INT)
+$status = $stmt->execute(); //実行
+if($status==false) {
+  sql_error($stmt);
+}else{
+  //mysql_insert_id()は直近でinsertした行のidを取得するphpの関数
+  redirect("mrdetail.php?MR_ID=".($pdo->lastInsertId()));
+}
 ?>
