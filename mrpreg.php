fa299920fbd161eda39882568f50caef5ea4bd65
加藤真也 <katonyonko@yahoo.co.jp>
2022-01-04 14:13:29 +0900

    mrpregのcssをちょっと修正

--- mrpreg.php
+++ mrpreg.php
@@ -21,7 +21,7 @@ if($status==false){
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="css/reset.css">
-  <link rel="stylesheet" href="css/main.css">
+  <link rel="stylesheet" href="css/mrpreg.css">
   <title>Document</title>
   <script src="js/jquery-2.1.3.min.js"></script>
 </head>
