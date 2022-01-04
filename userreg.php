40627ca3dd08abad5e665f505385de28cf94bf4f
INA <aquanouspana@gmail.com>
2022-01-04 20:44:06 +0900

    userreg.css作成

--- userreg.php
+++ userreg.php
@@ -7,18 +7,25 @@ session_start();
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width">
-<link rel="stylesheet" href="css/main.css" />
+<!-- <link rel="stylesheet" href="css/main.css" /> -->
+<link rel="stylesheet" href="css/reset.css">
+<link rel="stylesheet" href="css/userreg.css">
 <link href="css/bootstrap.min.css" rel="stylesheet">
 <style>div{padding: 10px;font-size:16px;}</style>
 <script src="js/func.js"></script>
 <title>会員登録</title>
 </head>
 <body>
-
 <header>
-  <nav class="navbar navbar-default">会員登録</nav>
+    <h1 class="header_colour"></h1>
 </header>
-
+<section id = "main">
+<div style="text-align: center">
+    <img src="./img/nbsk_c.png" alt="" >
+</div>
+<div style="text-align: center">
+  <h1 class="a">新規登録</h1>
+</div>
 <script>
 function confirm_test() {
     var select = confirm("本当に登録しますか？\n「OK」で登録\n「キャンセル」で登録中止");
@@ -29,13 +36,13 @@ function confirm_test() {
 <form action="userreg_act.php" method="POST" onsubmit="return confirm_test()">
 <table>
   <tr>
-    <td>登録情報</td>
-    <td>入力欄</td>
+    <th>登録情報</th>
+    <th>入力欄　　　</th>
   </tr>
   <tr>
-    <td>ID<sup>*</sup>:</td>
+    <td>　ID<sup>*</sup>:</td>
     <td>
-      <input type="text" name="USER_ID" placeholder="4文字以上の英数字を入力してください" required value="<?=$_SESSION["USER_ID"]?>" pattern="^([a-zA-Z0-9]{4,})$" title="4文字以上の英数字のみで入力して下さい"/>
+      <input type="text" name="USER_ID" placeholder="4文字以上の英数字を入力してください" required value="<?=$_SESSION["USER_ID"]?>" pattern="^([a-zA-Z0-9]{4,})$" title="4文字以上の英数字のみで入力して下さい" class="Column"/>
       <?php
       if ($_SESSION["duplicate"]==1) {
         echo "そのIDは使用できません、別のIDを指定してください";
@@ -45,21 +52,21 @@ function confirm_test() {
     </td>
   </tr>
   <tr>
-    <td>ユーザー名<sup>*</sup>:</td>
+    <td>　ユーザー名<sup>*</sup>:</td>
     <td>
-      <input type="text" name="USER_NAME" placeholder="好きな名前を設定してください" required value="<?=$_SESSION["USER_NAME"]?>"/>
+      <input type="text" name="USER_NAME" placeholder="好きな名前を設定してください" required value="<?=$_SESSION["USER_NAME"]?>" class="Column"/>
     </td>
   </tr>
   <tr>
-    <td>Password<sup>*</sup>:</td>
+    <td>　Password<sup>*</sup>:</td>
     <td>
-      <input type="password" id="input_pass" name="PASSWORD" placeholder="8文字以上の英数字を入力してください" required pattern="^([a-zA-Z0-9]{8,})$" title="8文字以上の英数字のみで入力して下さい"/><button id="btn_passview">表示</button>
+      <input type="password" id="input_pass" name="PASSWORD" placeholder="8文字以上の英数字を入力してください" required pattern="^([a-zA-Z0-9]{8,})$" title="8文字以上の英数字のみで入力して下さい" class="Column" /><button id="btn_passview">表示</button>
     </td>
   </tr>
   <tr>
-    <td>Password（再入力）<sup>*</sup>:</td>
+    <td>　Password(再入力)<sup>*</sup>:　</td>
     <td>
-      <input type="password" name="PASSWORD2" id="input_pass2" placeholder="Passwordを再入力してください" required pattern="^([a-zA-Z0-9]{8,})$" title="8文字以上の英数字のみで入力して下さい"/><button id="btn_passview2">表示</button>
+      <input type="password" name="PASSWORD2" id="input_pass2" placeholder="Passwordを再入力してください" required pattern="^([a-zA-Z0-9]{8,})$" title="8文字以上の英数字のみで入力して下さい" class="Column" ><button id="btn_passview2">表示</button>
       <?php
       if ($_SESSION["mismatch"]==1) {
         echo "Passwordが一致しません";
@@ -69,15 +76,15 @@ function confirm_test() {
     </td>
   </tr>
   <tr>
-    <td>e-mail<sup>*</sup>:</td>
+    <td>　e-mail<sup>*</sup>:</td>
     <td>
-      <input type="email" name="EMAIL" placeholder="example@test.ne.jp" value="<?=$_SESSION["EMAIL"]?>" required/>
+      <input type="email" name="EMAIL" placeholder="example@test.ne.jp" class="Column" value="<?=$_SESSION["EMAIL"]?>" required/>
     </td>
   </tr>
   <tr>
-    <td>性別:</td>
+    <td>　性別:</td>
     <td>
-      <select name="GENDER">
+      <select name="GENDER" class="Column">
         <option value="" <?php if (!isset($_SESSION["GENDER"])) {echo "selected";}?>>--選択してください--</option>
         <option value=1 <?php if ($_SESSION["GENDER"]==1) {echo "selected";}?>>男性</option>
         <option value=2 <?php if ($_SESSION["GENDER"]==2) {echo "selected";}?>>女性</option>
@@ -85,29 +92,34 @@ function confirm_test() {
     </td>
   </tr>
   <tr>
-    <td>生年月日:</td>
+    <td>　生年月日:</td>
     <td>
-      <input type="date" name="BIRTHDAY" value="<?=$_SESSION["BIRTHDAY"]?>"/>
+      <input type="date" name="BIRTHDAY" class="Column" value="<?=$_SESSION["BIRTHDAY"]?>"/>
     </td>
   </tr>
   <tr>
-    <td>住所:</td>
+    <td>　住所:</td>
     <td>
-      <input type="text" name="ADDRESS" value="<?=$_SESSION["ADDRESS"]?>"/>
+      <input type="text" name="ADDRESS" class="Column" value="<?=$_SESSION["ADDRESS"]?>"/>
     </td>
   </tr>
   <tr>
-    <td>Twitter:</td>
+    <td>　Twitter:</td>
     <td>
-      <input type="text" name="TWITTER" value="<?=$_SESSION["TWITTER"]?>"/>
+      <input type="text" name="TWITTER" class="Column" value="<?=$_SESSION["TWITTER"]?>"/>
     </td>
   </tr>
 </table>
 *は必須入力項目です。<br>
-<input type="submit" value="登録" />
-<button onclick="location.href='login.php'">戻る</button>
+<input type="submit" value="登録" class="button1" />
+<div style="text-align: center">
+  <button onclick="location.href='login.php'" class="button2">戻る</button>
+</div>
 </form>
-
+</section>
+<footer>
+  <h1 class="footer_colour"></h1>
+</footer>
 <script>
   //パスワードの表示非表示切り替え
   switching("input_pass","btn_passview");
