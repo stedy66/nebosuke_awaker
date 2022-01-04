3e1805d6a8ba0fee6d3562ed30d6452214888c26
加藤真也 <katonyonko@yahoo.co.jp>
2022-01-04 15:09:14 +0900

    mrdetailのcss途中まで

--- mrdetail.php
+++ mrdetail.php
@@ -31,45 +31,39 @@ else {
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/reset.css">
+  <link rel="stylesheet" href="css/mrdetail.css">
   <title>Document</title>
 </head>
-<body>
-  <!-- 画像 -->
-  <div style="text-align: center">
-    <img src="<?=$bg_url?>" alt="<?=$package["ROUTINE_NAME"]?>" title="<?=$package["ROUTINE_NAME"]?>" style="width: 70%;">
-</div>
-  <!-- ルーティン名 -->
-  <p style="width: 70%; text-align: center; margin-left: auto; margin-right: auto;"><?=$package["ROUTINE_NAME"]?></p>
-  <!-- テーブル -->
-  <table style="width: 70%; text-align: center; margin-left: auto; margin-right: auto;" cellspacing="0">
-      <tr>
-        <td style="background:yellow;border:1px solid;">ステップ数</td>
-        <td style="background:yellow;border:1px solid;">ステップ名</td>
-        <td style="background:yellow;border:1px solid;">時間</td>
-        <td style="background:yellow;border:1px solid;">説明</td>
-       </tr>
-       <?php
-        //２．ルーティンパッケージ抽出SQL作成
-        $stmt = $pdo->prepare("SELECT * FROM table1_3 LEFT JOIN table1_2 ON table1_3.STEP_ID=table1_2.STEP_ID WHERE table1_3.MR_ID=:MR_ID ORDER BY table1_3.SEQUENCE");
-        $stmt->bindValue(":MR_ID", $MR_ID, PDO::PARAM_INT);
-        $status = $stmt->execute();
 
-        //３．データ作成
-        if($status==false) {
-            sql_error($stmt);
-        }else{
-          while( $res = $stmt->fetch(PDO::FETCH_ASSOC)){
-            $view = '<tr>' ;
-            $view .= "<td style='background:#ffffe0;border:1px solid;'>".$res["SEQUENCE"]."</td>";
-            $view .= "<td style='background:#ffffe0;border:1px solid;'>".$res["STEP_NAME"]."</td>";
-            $view .= "<td style='background:#ffffe0;border:1px solid;'>".$res["PERIOD"]."min</td>";
-            $view .= "<td style='background:#ffffe0;border:1px solid; text-align: left;'>".$res["DESCRIPTION"]."</td>";
-            $view .= '<tr>';
-            echo $view;
-            }
-        }
-        ?>
-    </table>
+<body>
+  <header>ルーティン</header>
+  <!-- 画像、ルーティン -->
+  <div style="text-align: center; background-image: url('<?=$bg_url?>');" id="routine_name_bgi">
+    <p id="routine_name"><?=$package["ROUTINE_NAME"]?></p>
+  </div>
+  <!-- テーブル -->
+  <?php
+  //２．ルーティンパッケージ抽出SQL作成
+  $stmt = $pdo->prepare("SELECT * FROM table1_3 LEFT JOIN table1_2 ON table1_3.STEP_ID=table1_2.STEP_ID WHERE table1_3.MR_ID=:MR_ID ORDER BY table1_3.SEQUENCE");
+  $stmt->bindValue(":MR_ID", $MR_ID, PDO::PARAM_INT);
+  $status = $stmt->execute();
+
+  //３．データ作成
+  if($status==false) {
+      sql_error($stmt);
+  }else{
+    while( $res = $stmt->fetch(PDO::FETCH_ASSOC)){
+      $view = '<div class="step">' ;
+      $view .= "<p class='step_seq'>".$res["SEQUENCE"]."</p>";
+      $view .= "<p class='step_name'>".$res["STEP_NAME"]."</p>";
+      $view .= "<p class='step_period'>".$res["PERIOD"]."min</p>";
+      $view .= "<p text-align: left;' class='step_description'>".$res["DESCRIPTION"]."</p>";
+      $view .= '</div>';
+      echo $view;
+      }
+    }
+    ?>
     <!-- コメント -->
     <?php
       //nl2br関数はphpの改行コードをhtmlの改行タグに変換してくれる関数
