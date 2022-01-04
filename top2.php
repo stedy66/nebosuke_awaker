4b4c350628b7347ed5a85cbfc9ad57934e7e59eb
Shuichi Yabe <yeah6689@yahoo.co.jp>
2022-01-04 16:58:34 +0900

    top2style.css追加　top2ちょっと修正

--- top2.php
+++ top2.php
@@ -20,6 +20,7 @@ $user = $stmt->fetch();
 
 <!DOCTYPE html>
 <html lang="en">
+    <link href="css/top2style.css" rel="stylesheet">
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -27,6 +28,27 @@ $user = $stmt->fetch();
   <title>Document</title>
 </head>
 <body>
+    <!-- Header Start -->
+    <header class="site-header">
+      <div class="wrapper site-header__wrapper">
+        <a href="#" class="brand">ホーム</a>
+        <nav class="nav">
+          <button class="nav__toggle" aria-expanded="false" type="button">
+            menu
+          </button>
+          <!-- <ul class="nav__wrapper">
+            <li class="nav__item"><a href="#">Home</a></li>
+            <li class="nav__item"><a href="#">About</a></li>
+            <li class="nav__item"><a href="#">Services</a></li>
+            <li class="nav__item"><a href="#">Hire us</a></li>
+            <li class="nav__item"><a href="#">Contact</a></li>
+          </ul> -->
+        </nav>
+      </div>
+    </header>
+    <!-- Header End -->
+
+
   <?php
     if (isset($_SESSION["download"]) && $_SESSION["download"]==1) {
       echo "<p>ダウンロードしました。</p>";
