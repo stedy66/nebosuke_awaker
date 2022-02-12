7ebef1c823a8a7009af64cca9956e7e867dca9bd
keichooori <do.mo.oredesu@outlook.com>
2022-02-12 09:39:55 +0900

    更新機能完成
    データ型によるエラーも解決

--- user_update_act.php
+++ user_update_act.php
@@ -16,7 +16,7 @@ if ($icon == 1 ) {
 
 
 $stmt = $pdo->prepare("UPDATE table4 SET icon=:icon, EMAIL=:EMAIL,  ADDRESS=:ADDRESS, TWITTER=:TWITTER WHERE USER_ID=:USER_ID");
-$stmt->bindValue(':USER_ID', $id, PDO::PARAM_INT);
+$stmt->bindValue(':USER_ID', $id, PDO::PARAM_STR);
 $stmt->bindValue(':icon', $icon, PDO::PARAM_STR);
 $stmt->bindValue(':EMAIL', $_POST["EMAIL"], PDO::PARAM_STR);      //Integer（数値の場合 PDO::PARAM_INT)
 $stmt->bindValue(':ADDRESS', $_POST["ADDRESS"], PDO::PARAM_STR);      //Integer（数値の場合 PDO::PARAM_INT)
